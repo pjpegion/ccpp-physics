1ceb5d671f64544a6ebfd94e8a0edee27525fe23
Philip Pegion <Philip.Pegion@noaa.gov>
2019-06-10 14:53:08 +0000

    modifications for stochastic physics

--- physics/GFS_stochastics.F90
+++ physics/GFS_stochastics.F90
@@ -1,20 +1,38 @@
 !> \file GFS_stochastics.f90
 !! This file contains code previously in GFS_stochastics_driver.
 
+!>\defgroup gfs_stoch GFS Stochastics Main Module
+!! This module
+!! @ {
     module GFS_stochastics
 
       contains
 
-      subroutine GFS_stochastics_init ()
+#ifndef __PGI
+!> \section arg_table_GFS_stochastics_init Argument Table
+!! | local_name     | standard_name                                          | long_name                                                               | units    | rank |  type                 |   kind    | intent | optional |
+!! |----------------|--------------------------------------------------------|-------------------------------------------------------------------------|----------|------|-----------------------|-----------|--------|----------|
+!! | Model          | GFS_control_type_instance                              | Fortran DDT containing FV3-GFS model control parameters                 | DDT      |    0 | GFS_control_type      |           | inout  | F        |
+!! | errmsg         | ccpp_error_message                                     | error message for error handling in CCPP                                | none     |    0 | character             | len=*     | out    | F        |
+!! | errflg         | ccpp_error_flag                                        | error flag for error handling in CCPP                                   | flag     |    0 | integer               |           | out    | F        |
+!!
+#endif
+
+      subroutine GFS_stochastics_init (Model,errmsg,errflg)
+      use GFS_typedefs,       only: GFS_control_type, GFS_init_type
+      type(GFS_control_type),   intent(inout) :: Model
+      character(len=*),         intent(out)   :: errmsg
+integer,                  intent(out)   :: errflg
+      errmsg = ''
+      errflg = 0
+      call init_stochastic_physics(Model,errmsg,errflg)
+      print*,'finsihed with GFS_stochastics_init'
       end subroutine GFS_stochastics_init
 
       subroutine GFS_stochastics_finalize()
       end subroutine GFS_stochastics_finalize
 
-
-!>\defgroup gfs_stoch GFS Stochastics Physics Module
-!! This module
-!> @{
+#ifndef __PGI
 !> \section arg_table_GFS_stochastics_run Argument Table
 !! | local_name     | standard_name                                                             | long_name                                                    | units   | rank | type      |    kind   | intent | optional |
 !! |----------------|---------------------------------------------------------------------------|--------------------------------------------------------------|---------|------|-----------|-----------|--------|----------|
@@ -23,16 +41,9 @@
 !! | do_sppt        | flag_for_stochastic_surface_physics_perturbations                         | flag for stochastic surface physics perturbations            | flag    |    0 | logical   |           | in     | F        |
 !! | use_zmtnblck   | flag_for_mountain_blocking                                                | flag for mountain blocking                                   | flag    |    0 | logical   |           | in     | F        |
 !! | do_shum        | flag_for_stochastic_shum_option                                           | flag for stochastic shum option                              | flag    |    0 | logical   |           | in     | F        |
-!! | do_skeb        | flag_for_stochastic_skeb_option                                           | flag for stochastic skeb option                              | flag    |    0 | logical   |           | in     | F        |
 !! | zmtnblck       | level_of_dividing_streamline                                              | level of the dividing streamline                             | none    |    1 | real      | kind_phys | in     | F        |
 !! | sppt_wts       | weights_for_stochastic_sppt_perturbation                                  | weights for stochastic sppt perturbation                     | none    |    2 | real      | kind_phys | inout  | F        |
-!! | skebu_wts      | weights_for_stochastic_skeb_perturbation_of_x_wind                        | weights for stochastic skeb perturbation of x wind           | none    |    2 | real      | kind_phys | in     | F        |
-!! | skebv_wts      | weights_for_stochastic_skeb_perturbation_of_y_wind                        | weights for stochastic skeb perturbation of y wind           | none    |    2 | real      | kind_phys | in     | F        |
-!! | shum_wts       | weights_for_stochastic_shum_perturbation                                  | weights for stochastic shum perturbation                     | none    |    2 | real      | kind_phys | in     | F        |
-!! | sppt_wts_inv   | weights_for_stochastic_sppt_perturbation_flipped                          | weights for stochastic sppt perturbation, flipped            | none    |    2 | real      | kind_phys | inout  | F        |
-!! | skebu_wts_inv  | weights_for_stochastic_skeb_perturbation_of_x_wind_flipped                | weights for stochastic skeb perturbation of x wind, flipped  | none    |    2 | real      | kind_phys | inout  | F        |
-!! | skebv_wts_inv  | weights_for_stochastic_skeb_perturbation_of_y_wind_flipped                | weights for stochastic skeb perturbation of y wind, flipped  | none    |    2 | real      | kind_phys | inout  | F        |
-!! | shum_wts_inv   | weights_for_stochastic_shum_perturbation_flipped                          | weights for stochastic shum perturbation, flipped            | none    |    2 | real      | kind_phys | inout  | F        |
+!! | shum_wts       | weights_for_stochastic_shum_perturbation                                  | weights for stochastic shum perturbation                     | none    |    2 | real      | kind_phys | inout  | F        |
 !! | diss_est       | dissipation_estimate_of_air_temperature_at_model_layers                   | dissipation estimate model layer mean temperature            | K       |    2 | real      | kind_phys | in     | F        |
 !! | ugrs           | x_wind                                                                    | zonal wind                                                   | m s-1   |    2 | real      | kind_phys | in     | F        |
 !! | vgrs           | y_wind                                                                    | meridional wind                                              | m s-1   |    2 | real      | kind_phys | in     | F        |
@@ -57,25 +68,29 @@
 !! | dsnow_cpl      | tendency_of_lwe_thickness_of_snow_amount_for_coupling                     | change in show_cpl (coupling_type)                           | m       |    1 | real      | kind_phys | in     | F        |
 !! | errmsg         | ccpp_error_message                                                        | error message for error handling in CCPP                     | none    |    0 | character | len=*     | out    | F        |
 !! | errflg         | ccpp_error_flag                                                           | error flag for error handling in CCPP                        | flag    |    0 | integer   |           | out    | F        |
+!! | Model          | GFS_control_type_instance                                                 | Fortran DDT containing FV3-GFS model control parameters     | DDT      |    0 | GFS_control_type      |        | inout  | F        |
+!! | Coupling       | GFS_coupling_type_instance                                                | Fortran DDT containing FV3-GFS fields to/from coupling with other components | DDT      |    0 | GFS_coupling_type     |           | inout  | F        |
 !!
-!>\section gfs_stochy_general GFS_stochastics_run General Algorithm
-!! This is the GFS stochastic physics driver.
-!! Routines are called prior to radiation and physics steps to handle:
-!! -# sets up various time/date variables
-!! -# sets up various triggers
-!! -# defines random seed indices for radiation (in a reproducible way)
-!! -# interpolates coefficients for prognostic ozone calculation
-!! -# performs surface data cycling via the GFS gcycle routine
-      subroutine GFS_stochastics_run (im, km, do_sppt, use_zmtnblck, do_shum, do_skeb,   &
-                                      zmtnblck, sppt_wts, skebu_wts, skebv_wts, shum_wts,&
-                                      sppt_wts_inv, skebu_wts_inv, skebv_wts_inv,        &
-                                      shum_wts_inv, diss_est,                            &
+#endif
+!-------------------------------------------------------------------------
+! GFS stochastic_driver
+!-------------------------------------------------------------------------
+!    routine called prior to radiation and physics steps to handle:
+!      1) sets up various time/date variables
+!      2) sets up various triggers
+!      3) defines random seed indices for radiation (in a reproducible way)
+!      5) interpolates coefficients for prognostic ozone calculation
+!      6) performs surface data cycling via the GFS gcycle routine
+!-------------------------------------------------------------------------
+      subroutine GFS_stochastics_run (im, km, do_sppt, use_zmtnblck, do_shum,            &
+                                      zmtnblck, sppt_wts, shum_wts, diss_est,            &
                                       ugrs, vgrs, tgrs, qgrs, gu0, gv0, gt0, gq0, dtdtr, &
                                       rain, rainc, tprcp, totprcp, cnvprcp,              &
                                       totprcpb, cnvprcpb, cplflx,                        &
                                       rain_cpl, snow_cpl, drain_cpl, dsnow_cpl,          &
-                                      errmsg, errflg)
+                                      errmsg, errflg,Model,Coupling)
 
+         use GFS_typedefs,       only: GFS_control_type, GFS_coupling_type
          use machine,               only: kind_phys
 
          implicit none
@@ -85,21 +100,11 @@
          logical,                               intent(in)    :: do_sppt
          logical,                               intent(in)    :: use_zmtnblck
          logical,                               intent(in)    :: do_shum
-         logical,                               intent(in)    :: do_skeb
-         !logical,                               intent(in)    :: isppt_deep
          real(kind_phys), dimension(1:im),      intent(in)    :: zmtnblck
          ! sppt_wts only allocated if do_sppt == .true.
          real(kind_phys), dimension(:,:),       intent(inout) :: sppt_wts
-         ! skebu_wts, skebv_wts only allocated if do_skeb == .true.
-         real(kind_phys), dimension(:,:),       intent(in)    :: skebu_wts
-         real(kind_phys), dimension(:,:),       intent(in)    :: skebv_wts
          ! shum_wts only allocated if do_shum == .true.
          real(kind_phys), dimension(:,:),       intent(in)    :: shum_wts
-         ! inverse/flipped weights are always allocated
-         real(kind_phys), dimension(1:im,1:km), intent(inout) :: sppt_wts_inv
-         real(kind_phys), dimension(1:im,1:km), intent(inout) :: skebu_wts_inv
-         real(kind_phys), dimension(1:im,1:km), intent(inout) :: skebv_wts_inv
-         real(kind_phys), dimension(1:im,1:km), intent(inout) :: shum_wts_inv
          real(kind_phys), dimension(1:im,1:km), intent(in)    :: diss_est
          real(kind_phys), dimension(1:im,1:km), intent(in)    :: ugrs
          real(kind_phys), dimension(1:im,1:km), intent(in)    :: vgrs
@@ -125,13 +130,10 @@
          ! drain_cpl, dsnow_cpl only allocated if do_sppt == .true.
          real(kind_phys), dimension(:),         intent(in)    :: drain_cpl
          real(kind_phys), dimension(:),         intent(in)    :: dsnow_cpl
-         ! tconvtend ... vconvtend only allocated if isppt_deep == .true.
-         !real(kind_phys), dimension(:,:),       intent(in)    :: tconvtend
-         !real(kind_phys), dimension(:,:),       intent(in)    :: qconvtend
-         !real(kind_phys), dimension(:,:),       intent(in)    :: uconvtend
-         !real(kind_phys), dimension(:,:),       intent(in)    :: vconvtend
          character(len=*),                      intent(out)   :: errmsg
          integer,                               intent(out)   :: errflg
+         type(GFS_control_type),   intent(inout) :: Model
+         type(GFS_coupling_type),   intent(inout) :: Coupling
 
          !--- local variables
          integer :: k, i
@@ -140,6 +142,9 @@
          ! Initialize CCPP error handling variables
          errmsg = ''
          errflg = 0
+      print*,'call stochastic_physics_run'
+
+         call stochastic_physics_run(Model, Coupling, errmsg, errflg)
 
          if (do_sppt) then
            do k=1,km
@@ -164,27 +169,14 @@
                if (use_zmtnblck)then
                   sppt_wts(i,k)=(sppt_wts(i,k)-1)*sppt_vwt+1.0
                endif
-               sppt_wts_inv(i,km-k+1)=sppt_wts(i,k)
-
-               !if(isppt_deep)then
-
-                ! upert = (gu0(i,k) - ugrs(i,k) - uconvtend(i,k)) + uconvtend(i,k) * sppt_wts(i,k)
-                ! vpert = (gv0(i,k) - vgrs(i,k) - vconvtend(i,k)) + vconvtend(i,k) * sppt_wts(i,k)
-                ! tpert = (gt0(i,k) - tgrs(i,k) - dtdtr(i,k) - tconvtend(i,k)) + tconvtend(i,k) * sppt_wts(i,k)
-                ! qpert = (gq0(i,k) - qgrs(i,k) - qconvtend(i,k)) + qconvtend(i,k) * sppt_wts(i,k)
-
-               !else
 
                upert = (gu0(i,k) - ugrs(i,k))   * sppt_wts(i,k)
                vpert = (gv0(i,k) - vgrs(i,k))   * sppt_wts(i,k)
                tpert = (gt0(i,k) - tgrs(i,k) - dtdtr(i,k)) * sppt_wts(i,k)
                qpert = (gq0(i,k) - qgrs(i,k)) * sppt_wts(i,k)
-
-               !endif
-
                gu0(i,k)  = ugrs(i,k)+upert
                gv0(i,k)  = vgrs(i,k)+vpert
-
+   
                !negative humidity check
                qnew = qgrs(i,k)+qpert
                if (qnew >= 1.0e-10) then
@@ -193,57 +185,34 @@
                endif
              enddo
            enddo
-
-           !if(isppt_deep)then
-           !  tprcp(:) = tprcp(:) + (sppt_wts(:,15) - 1 )*rainc(:)
-           !  totprcp(:)  = totprcp(:)  + (sppt_wts(:,15) - 1 )*rainc(:) 
-           !  cnvprcp(:)  = cnvprcp(:)  + (sppt_wts(:,15) - 1 )*rainc(:)
-           !!  ! bucket precipitation adjustment due to sppt
-           !  totprcpb(:) = totprcpb(:)  + (sppt_wts(:,15) - 1 )*rainc(:)
-           !  cnvprcpb(:) = cnvprcpb(:)  + (sppt_wts(:,15) - 1 )*rainc(:)
-
-           !  if (cplflx) then !Need to make proper adjustments for deep convection only perturbations
-           !    rain_cpl(:) = rain_cpl(:) + (sppt_wts(:,15) - 1.0)*drain_cpl(:)
-           !    snow_cpl(:) = snow_cpl(:) + (sppt_wts(:,15) - 1.0)*dsnow_cpl(:)
-           !  endif
-
-           !else
-
            ! instantaneous precip rate going into land model at the next time step
-           tprcp(:) = sppt_wts(:,15)*tprcp(:)
-           totprcp(:) = totprcp(:) + (sppt_wts(:,15) - 1 )*rain(:)
+           tprcp(:) = sppt_wts(:,30)*tprcp(:)
+           totprcp(:) = totprcp(:) + (sppt_wts(:,30) - 1 )*rain(:)
            ! acccumulated total and convective preciptiation
-           cnvprcp(:) = cnvprcp(:) + (sppt_wts(:,15) - 1 )*rainc(:)
+           cnvprcp(:) = cnvprcp(:) + (sppt_wts(:,30) - 1 )*rainc(:)
            ! bucket precipitation adjustment due to sppt
-           totprcpb(:) = totprcpb(:) + (sppt_wts(:,15) - 1 )*rain(:)
-           cnvprcpb(:) = cnvprcpb(:) + (sppt_wts(:,15) - 1 )*rainc(:)
+           totprcpb(:) = totprcpb(:) + (sppt_wts(:,30) - 1 )*rain(:)
+           cnvprcpb(:) = cnvprcpb(:) + (sppt_wts(:,30) - 1 )*rainc(:)
 
             if (cplflx) then
-               rain_cpl(:) = rain_cpl(:) + (sppt_wts(:,15) - 1.0)*drain_cpl(:)
-               snow_cpl(:) = snow_cpl(:) + (sppt_wts(:,15) - 1.0)*dsnow_cpl(:)
+               rain_cpl(:) = rain_cpl(:) + (sppt_wts(:,30) - 1.0)*drain_cpl(:)
+               snow_cpl(:) = snow_cpl(:) + (sppt_wts(:,30) - 1.0)*dsnow_cpl(:)
             endif
-
-           !endif
-
+            print*,' in stoch physics'
+           do k=1,km
+              print*,k,sppt_wts(1,k),gt0(1,k)
+           enddo
+         
          endif
 
          if (do_shum) then
            do k=1,km
              gq0(:,k) = gq0(:,k)*(1.0 + shum_wts(:,k))
-             shum_wts_inv(:,km-k+1) = shum_wts(:,k)
            end do
          endif
          
-         if (do_skeb) then
-           do k=1,km
-             gu0(:,k) = gu0(:,k)+skebu_wts(:,k)*(diss_est(:,k))
-             gv0(:,k) = gv0(:,k)+skebv_wts(:,k)*(diss_est(:,k))
-             skebu_wts_inv(:,km-k+1) = skebu_wts(:,k)
-             skebv_wts_inv(:,km-k+1) = skebv_wts(:,k)
-           enddo
-         endif
 
       end subroutine GFS_stochastics_run
 
     end module GFS_stochastics
-!> @}
+!! @}
